b54a7850c0d6f5537d97664f857b2b46372abbfa
luxin <398873800@qq.com>
2020-04-02 15:23:19 +0800

    init flutter_qukit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'example/gradient_button.dart';
+
+void main() => runApp(KitApp());
+
+class KitApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter qukit',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: KitHomePage(),
+    );
+  }
+}
+
+class KitHomePage extends StatefulWidget {
+  KitHomePage({Key key}) : super(key: key);
+  @override
+  _KitHomePageState createState() => _KitHomePageState();
+}
+
+class _KitHomePageState extends State<KitHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('flutter qukit'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => GradientButtonRoute(),
+                ));
+              },
+              child: Text(
+                'GradientButton',
+              )
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
